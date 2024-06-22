185e8a8fec7158af4f59284b1f514639cbaddab5
aman <saifiamaan445@gmail.com>
2024-06-22 23:08:01 +0530

    Counter Component

---
+++ class22/Counter/script.js
@@ -0,0 +1,30 @@
+const IncrementBtn = document.querySelector('#plus');
+const DecrementBtn = document.querySelector('#minus');
+const CountElement = document.querySelector('#count');
+const InputElement = document.querySelector('input');
+
+let count = 0;
+let incrementDecrementBy = 1;
+
+InputElement.addEventListener('change',function(){
+    const value = parseInt(InputElement.value);
+    if(value){
+        incrementDecrementBy = value;
+    }else{
+        incrementDecrementBy = 1;
+    }
+})
+
+IncrementBtn.addEventListener('click',function(e){
+    count+=incrementDecrementBy;
+    CountElement.innerText = "Counter : " + count;
+})
+
+
+DecrementBtn.addEventListener('click',function(e){
+    if(count-incrementDecrementBy < 0){
+        return;
+    }
+    count-=incrementDecrementBy;
+    CountElement.innerText = "Counter : " + count;
+})
