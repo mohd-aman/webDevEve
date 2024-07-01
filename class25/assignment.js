eabf34693ccfc001d263edab5464bab2f3b1fdf9
aman <saifiamaan445@gmail.com>
2024-07-01 22:55:51 +0530

    assignment

--- class25/assignment.js
+++ class25/assignment.js
@@ -1,34 +1,34 @@
 
 
-let obj = {
-    name:'person',
-    age:100,
-    fxn:function(){
+// let obj = {
+//     name:'person',
+//     age:100,
+//     fxn:function(){
         
-        return ()=>{
-            console.log(this.name);
-        }
-    }
-}
+//         return ()=>{
+//             console.log(this.name);
+//         }
+//     }
+// }
 
-let returnedInnerFxn = obj.fxn();
-returnedInnerFxn(); 
+// let returnedInnerFxn = obj.fxn();
+// returnedInnerFxn(); 
 
 
 
-let obj2 = {
-    name:'person',
-    age:100,
-    fxn:function(){
-        console.log(this.age)
-        const arrowFxn= ()=>{
-            console.log(this.name);
-        }
-        arrowFxn();
-    }
-}
+// let obj2 = {
+//     name:'person',
+//     age:100,
+//     fxn:function(){
+//         console.log(this.age)
+//         const arrowFxn= ()=>{
+//             console.log(this.name);
+//         }
+//         arrowFxn();
+//     }
+// }
 
-obj2.fxn();
+// obj2.fxn();
 
-let fxnExpression = obj2.fxn;
-fxnExpression();
+// let fxnExpression = obj2.fxn;
+// fxnExpression();
