92ea1081f0cede0bfadd0da7e0a48f52aefd9a98
aman <saifiamaan445@gmail.com>
2024-07-23 23:14:37 +0530

    styling update

--- movies-app/src/Components/Movies.jsx
+++ movies-app/src/Components/Movies.jsx
@@ -5,7 +5,6 @@ export default function Movies(){
                 <img className="h-[15rem] w-[10rem]" src="https://image.tmdb.org/t/p/original/ga4OLm4qLxPqKLMzjJlqHxVjst3.jpg&quot"/>
                 <p className="absolute bottom-0 text-white">Bad Boys: Ride or Die</p>
             </div>
-            
         </div>
     )
 }
