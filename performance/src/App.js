e2ad0ae73067f925c0ebb66ee8738899bd8d4413
aman <saifiamaan445@gmail.com>
2024-08-10 22:09:01 +0530

    suspense and lazy

--- performance/src/App.js
+++ performance/src/App.js
@@ -1,46 +1,24 @@
-import './App.css';
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import HomePage from './Components/HomePage';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(()=>import('./Components/HomePage'));
+const AboutPage = lazy(()=>import('./Components/AboutPage'));
+const ContactPage = lazy(()=>import('./Components/ContactPage'))
 
 function App() {
-
-  const [ContactPage,setConctactPage] = useState(null);
-  const [AboutPage,setAboutPage] = useState(null);
-
-  const loadAbout = ()=>{
-    import('./Components/AboutPage')
-    .then((module)=>{
-      setAboutPage(()=>module.default);
-    })
-  }
-
-  const laodContact = ()=>{
-    import('./Components/ContactPage')
-    .then((module)=>{
-      setConctactPage(()=>module.default);
-    })
-  }
   return (
-   <BrowserRouter>
-      <ul>
-            <li>
-                <Link to="/" >Home</Link>
-            </li>
-            <li>
-                <Link to='/about' onClick={loadAbout}>About Us</Link>
-            </li>
-            <li>
-                <Link to='/contact' onClick={laodContact}>Contact</Link>
-            </li>
-        </ul>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/contact' element={ContactPage?<ContactPage/>:<div>...Loading</div>}/>
-        <Route path='/about' element={AboutPage?<AboutPage/>:<div>...Loading</div>}/>
-      </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Suspense fallback={<div>...Loading</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
